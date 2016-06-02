b38dbf24ae1b254b82ac42e783d86e2879e02b47
Louis "Kureuil" Person <lait.kureuil@gmail.com>
2016-06-02 16:14:38 -0700

    Allow paged retrieval of resources

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -36,17 +36,46 @@ func Init(driver string, user string, password string, host string, port string,
 	return nil
 }
 
-// FetchEntities fetch entities from the datastore with the given constraints
-func FetchEntities(out interface{}, where ...interface{}) *herr.Error {
-	if err := Conn.Find(out, where...).Error; err != nil {
-		e := herr.Error{
+// CountEntities count entities from the datastore with the given constraints
+func CountEntities(model interface{}, out interface{}, where interface{}, args ...interface{}) *herr.Error {
+	conn := Conn.Model(model)
+	if where != nil {
+		conn = conn.Where(where, args...)
+	}
+	if err := conn.Count(out).Error; err != nil {
+		return &herr.Error{
 			ID:     "database-error",
 			Status: "500",
 			Title:  "Database Error",
 			Detail: err.Error(),
 		}
-		return &e
 	}
+	return nil
+}
+
+// FetchEntities fetch entities from the datastore with the given constraints
+func FetchEntities(out interface{}, where ...interface{}) *herr.Error {
+	if err := Conn.Find(out, where...).Error; err != nil {
+		return &herr.Error{
+			ID:     "database-error",
+			Status: "500",
+			Title:  "Database Error",
+			Detail: err.Error(),
+		}
+	}
+	return nil
+}
+
+// FetchEntities fetch entities from the datastore with the given constraints
+func FetchPagedEntities(out interface{}, limit int, offset int, where ...interface{}) *herr.Error {
+	if err := Conn.Limit(limit).Offset(offset).Find(out, where...).Error; err != nil {
+		return &herr.Error{
+			ID:     "database-error",
+			Status: "500",
+			Title:  "Database Error",
+			Detail: err.Error(),
+		}
+	}	
 	return nil
 }
 
